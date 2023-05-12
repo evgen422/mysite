3a47744b5da1e175223edfaeeb5402b72d04e06f
evgen422 <evgen422@gmail.com>
2023-05-12 16:55:20 +0400

    multi-thread functionality implemented3

--- opencv/views.py
+++ opencv/views.py
@@ -24,47 +24,18 @@ from threading import Thread
 from opencv.apps import frame_queue
 
 
-
-
 # Define the view that renders the HTML template and streams the video
 def video_feed(request):
     return StreamingHttpResponse(show_frame(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 def index(request):
     return render(request, 'opencv.html')
-        
 
-'''
-def gen_frames():
-    url1 = 'http://136.169.226.81/1554451338BMM242/tracks-v1/mono.m3u8?token='
-    token = get_token()
-    url = (f'{url1}{token}')
-    print(url)
-    capture = cv2.VideoCapture(src)
-    # Start the thread to read frames from the video stream
-    thread = Thread(target=self.update, args=())
-    thread.daemon = True
-    thread.start()
-
-def update():
-    print('update funct started...')
-    # Read the next frame from the stream in a different thread
-    while True:
-        if self.capture.isOpened():
-            capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-            capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-            (self.status, self.frame) = self.capture.read()
-        time.sleep(.01)
-'''
 def show_frame():
-    # Display frames in main program
-    # Resize the frame to 480x320
-    #resized_frame = cv2.resize(frame, (480, 320))
-     # Encode the frame as a JPEG image
-    #ret, buffer = cv2.imencode('.jpg', resized_frame)
     # Convert the encoded image to a byte string and yield it as a response
     while True:
         if not frame_queue.empty():
+            fps_counter()
             frame = frame_queue.get()
             resized_frame = cv2.resize(frame, (480, 320))
             # Encode the frame as a JPEG image
@@ -75,90 +46,19 @@ def show_frame():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + encoded_frame + b'\r\n')
 
-'''
-def get_token(self):  
-    url = "http://maps.ufanet.ru/ufa" 
-    options = Options()
-    options.add_argument("--headless")
-    #options.add_argument("--no-sandbox")
-    options.headless = True
-
-    driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
-
-    driver.get(url)
-    print(driver.title)
-
-
-    # Find the element with the event listener
-    element = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[1]/div[2]/div[3]/img[227]')
-    #element = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[1]/div[2]/div[3]/img[227]')
-    time.sleep(1)
-
-    # Click the element to trigger the event listener
-    action = ActionChains(driver)
-    action.double_click(element).perform()
-    time.sleep(1)
-
-    # Get the location of the element
-    location = element.location
-
-    # Move the mouse 100 pixels up from the element
-    action = ActionChains(driver)
-
-    for i in range(100, 105):
-        i = 0-i
-        action.move_to_element_with_offset(element, 0, 0)
-        action.move_to_element_with_offset(element, 0, i)
-
-        # Click on the element
-        action.click().perform()
-    time.sleep(2)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    div = soup.find('div', {'class': 'ModalBodyPlayer'})
-    video = div.find('iframe')
-    link = video.attrs.get('src')
-    link = link.split('token=')
-    link = link[1]
-    link = link.split('&mute=true')
-    token = link[0]
-    print(token)
-    driver.quit()
-    return token
-'''
-
-
-
-    # initialize OpenCV's video stream
-    
-'''    video = cv2.VideoCapture(url)
-    if not video.isOpened():
-        print("Error opening video stream or file")
-
-    video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    if not video.isOpened():
-        print("Error opening video stream or file")
-
-    while True:
-        success, frame = video.read()
-        if not success:
-            break
-        else:
-            # Resize the frame to 480x320
-            frame = cv2.resize(frame, (480, 320))
-             # Encode the frame as a JPEG image
-            ret, buffer = cv2.imencode('.jpg', frame)
-            # Convert the encoded image to a byte string and yield it as a response
-            frame = buffer.tobytes()
-            time.sleep(0.04)
-            #if cv2.waitKey(50) & 0xFF == ord('q'):
-            #    break
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') '''
-
-
+time_start = dt.datetime.now()
+i = 0
+def fps_counter():
+    global i
+    global time_start
+    i = i+1
+    time_cycle = dt.datetime.now()
+    time_gap = time_cycle - time_start
+    time_gap_ms = time_gap.total_seconds() * 1000
+    if time_gap_ms > 10000:
+        print('fps ', i)
+        i = 0
+        time_start = dt.datetime.now()
 
 '''
 This code defines a class `VideoStreamWidget` that initializes a video stream and displays its frames. It uses OpenCV library to handle the video stream.
